0af5d460ec01ef59282c36a2489936e6dc646274
Maksim Dreev <dreev24@yandex.ru>
2024-12-01 17:32:28 +0300

    fixed after review - 2

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 from api.recipes.urls import recipe_urls
 from api.users.urls import users_urls
@@ -9,4 +10,8 @@ app_name = 'api'
 urlpatterns = [
     path('', include(users_urls)),
     path('', include(recipe_urls)),
+    path('docs/',
+        TemplateView.as_view(template_name='docs/redoc.html'),
+        name='redoc'
+        ),
 ]
--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -2,19 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
-from django.views.static import serve
+
 
 from api.recipes.views import short_url
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/', include('api.urls', namespace='api')),
-    path(
-        'redoc/',
-        TemplateView.as_view(template_name='redoc.html'),
-        name='redoc'
-    ),
     path('s/<int:pk>/', short_url, name='short_url')
 ]
 
@@ -23,14 +17,3 @@ if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
-
-    urlpatterns += [
-        path(
-            "redoc/openapi-schema.yml",
-            serve,
-            {
-                "path": "openapi-schema.yml",
-                "document_root": settings.BASE_DIR.parent / "docs",
-            },
-        ),
-    ]

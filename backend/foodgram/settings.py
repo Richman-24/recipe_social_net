e1a302207e870311f9304f21b559705028edced4
Maksim Dreev <dreev24@yandex.ru>
2024-12-02 16:11:10 +0300

    fix docs 5 - pep8

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'foodgram.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [], # os.path.join(BASE_DIR, 'api', 'docs')
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
